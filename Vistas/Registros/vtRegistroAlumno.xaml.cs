3819a7ea7a1205cf6a617d66205bd98ff981eee3
Jarly Hernandez Romero <jarlyhdezromero@hotmail.com>
2025-03-15 12:46:43 -0600

    Ventanas Registros

---
+++ Vistas/Registros/vtRegistroAlumno.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LenguaVivaCliente.Vistas.Registros
+{
+    /// <summary>
+    /// Lógica de interacción para vtRegistroAlumno.xaml
+    /// </summary>
+    public partial class vtRegistroAlumno : Page
+    {
+        public vtRegistroAlumno()
+        {
+            InitializeComponent();
+        }
+    }
+}
