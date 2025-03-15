9766316a44eeab48886cb17ab2dcb80ec3b6c39e
Jarly Hernandez Romero <jarlyhdezromero@hotmail.com>
2025-03-15 15:03:10 -0600

    aaa

--- Vistas/Registros/vtRegistroAdministrador.xaml.cs
+++ Vistas/Registros/vtRegistroAdministrador.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ServicioContrato;
+using LVServicio = LenguaVivaCliente.ServicioLenguaViva.IGestionUsuarios;
+
 
 namespace LenguaVivaCliente.Vistas.Registros
 {
@@ -21,44 +24,17 @@ namespace LenguaVivaCliente.Vistas.Registros
     /// </summary>
     public partial class vtRegistroAdministrador : Page
     {
-        private IGestionUsuarios _servicio;
+        private ServicioContrato.IGestionUsuarios _servicio;
 
         public vtRegistroAdministrador()
         {
             InitializeComponent();
-            _servicio = new IGestionUsuarios();
+            _servicio = new ImplementacionServicio();
         }
 
         private void BtnAceptar_Click(object sender, RoutedEventArgs e)
         {
-            string nombreUsuario = txtUsuario.Text;
-            string contrasenia = txtContrasenia.Password;
-            string nombre = txtNombre.Text;
-            string apellidos = txtApellidos.Text;
-            string direccion = txtDireccion.Text;
-            string email = txtCorreo.Text;
-
-            if (string.IsNullOrWhiteSpace(nombreUsuario) || string.IsNullOrWhiteSpace(contrasenia) ||
-                string.IsNullOrWhiteSpace(nombre) || string.IsNullOrWhiteSpace(apellidos) ||
-                string.IsNullOrWhiteSpace(direccion) || string.IsNullOrWhiteSpace(email))
-            {
-                MessageBox.Show("Todos los campos son obligatorios", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            bool registrado = _servicio.RegistrarAdminsitrador(nombreUsuario, contrasenia, nombre, apellidos, direccion, email);
-
-            if (registrado) 
-            {
-                MessageBox.Show("Administrador registrado con éxito", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
-                LimpiarCampos();
-
-            }
-            else
-            {
-                MessageBox.Show("Error al registrar el administrador", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-
-            }
+           
 
 
         }
